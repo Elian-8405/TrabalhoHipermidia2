5795ee126601c7b9d6b28d2989d0d8b61a1c7d21
Elia Silva <elianribeiro022@gmail.com>
2025-01-22 20:26:03 -0300

    Last commit before send

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Smooth Scroll for Anchors
+ 
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    // Smooth Scroll for GoToReviews button
+ 
     document.getElementById('goToReviews').addEventListener('click', function (e) {
         e.preventDefault();
         document.querySelector('#review_section').scrollIntoView({
@@ -17,12 +17,12 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    // Slide Logic
+  
     let currentSlide = 0;
     const slides = document.querySelector('.slides');
     const totalSlides = document.querySelectorAll('.slide').length;
 
-    // Function to change slides
+ 
     function moveSlide(step) {
         currentSlide += step;
 
@@ -35,29 +35,19 @@ document.addEventListener('DOMContentLoaded', function () {
         slides.style.transform = `translateX(-${currentSlide * 100}%)`;
     }
 
-    // Set up the buttons
+    
     document.getElementById('prevBtn').addEventListener('click', function () {
-        moveSlide(-1); // Move to the previous slide
+        moveSlide(-1); 
     });
 
     document.getElementById('nextBtn').addEventListener('click', function () {
-        moveSlide(1); // Move to the next slide
+        moveSlide(1); 
     });
 
-    // Initialize the first slide
+    
     moveSlide(0);
 
-    const menuToggle = document.getElementById('menu-toggle');
-    const mobileNavContainer = document.getElementById('mobile_nav_container');
-
-    menuToggle.addEventListener('click', function () {
-        // Alterna a classe active no menu
-        mobileNavContainer.classList.toggle('active');
-
-        // Alterna o ícone do hambúrguer entre aberto e fechado
-        menuToggle.classList.toggle('open');
-    });
-
+  
 
 });
 
